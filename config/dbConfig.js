18030fc77b2e2f4e1f72a9366bf4aa26de5109bd
Ujjawal Modanwal <51051403+ujjawalmodanwal@users.noreply.github.com>
2023-05-07 16:50:03 +0530

    updated database credentials

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -2,10 +2,10 @@ const { Pool } = require('pg');
 
 const pool = new Pool ({
 	user: 'library_database_user',
-	password:'VbQe2qGhMyrNVQp2a6ELN2YSXzZ5JoLz',
-	host:'dpg-ceqhff02i3mov0hld820-a.singapore-postgres.render.com', 
+	password:'4deLBV31gg0gxSZZ4YtPCypftN7cI0mN',
+	host:'dpg-chboh5ik728tp9ffq5mg-a.singapore-postgres.render.com', 
 	port:'5432',
-	database:'library_database',
+	database:'library_database_rwej',
 	ssl:true,
 })
 
